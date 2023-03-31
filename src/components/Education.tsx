e761258ae2de9191732fb1189e1f502c1579eea4
Marco <dispotomarco@gmail.com>
2023-03-31 20:39:44 +0200

    Various Fixes

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -3,15 +3,11 @@ import { EduProps } from "../types/types";
 const Education = ({ props }: { props: EduProps }) => {
   return (
     <>
-      <p className="my-0 py-0 text-tblue dark:text-light text-sm">
-        {props.date}
-      </p>
+      <p className="text-sm">{props.date}</p>
       <h3 className="font-bold text-xl text-tblue dark:text-light">
         {props.school}
       </h3>
-      <p className="my-0 py-0 text-tblue dark:text-light mb-5">
-        {props.description}
-      </p>
+      <p className="mb-5">{props.description}</p>
     </>
   );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,13 @@
-import { useState } from "react";
-
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { AppState } from "../types/types";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleHalfStroke, faBars } from "@fortawesome/free-solid-svg-icons";
+import { faCircleHalfStroke } from "@fortawesome/free-solid-svg-icons";
 
 import Picture from "./Picture";
 import ContactArea from "./ContactArea";
 
-import itFlag from "../assets/it 2.svg";
-import gbFlag from "../assets/gb 2.svg";
-import { actions } from "../store/store";
-
 const Header = ({ onToggleDarkMode }: { onToggleDarkMode: () => void }) => {
-  const dispatch = useDispatch();
   const { isDarkMode } = useSelector((state: AppState) => state);
 
   return (
@@ -28,59 +21,6 @@ const Header = ({ onToggleDarkMode }: { onToggleDarkMode: () => void }) => {
           <div
             className={`absolute flex flex-col top-5 right-5 xl:top-1/2 xl:-translate-x-1/2 xl:right-0`}
           >
-            {/* <button
-              className="items-center text-tblue dark:text-light"
-              style={{ justifySelf: "flex-end" }}
-              onClick={() => setIsOpen(!isOpen)}
-            >
-              <FontAwesomeIcon icon={faBars} size="2x" fixedWidth />
-            </button>
-            {isOpen && (
-              <div className="flex flex-col mt-3">
-                <button
-                  className="border-none bg-transparent text-tblue dark:text-orange cursor-pointer transition-all 1s ease-in-out"
-                  onClick={onToggleDarkMode}
-                  style={{ justifySelf: "flex-end" }}
-                >
-                  {isDarkMode ? (
-                    <FontAwesomeIcon
-                      icon={faCircleHalfStroke}
-                      flip="horizontal"
-                      className="w-[1.75rem] h-[1.75rem]"
-                    />
-                  ) : (
-                    <FontAwesomeIcon
-                      icon={faCircleHalfStroke}
-                      className="w-[1.75rem] h-[1.75rem]"
-                    />
-                  )}
-                </button>
-                {language === "en" ? (
-                  <button
-                    className="flex justify-center items-center mt-3"
-                    onClick={() => handleSwitchLanguage("it")}
-                  >
-                    <img
-                      src={itFlag}
-                      alt=""
-                      className={`rounded-full w-[1.75rem] h-[1.75rem] border-2 border-tblue dark:border-light`}
-                    />
-                  </button>
-                ) : (
-                  <button
-                    className="flex justify-center items-center mt-3"
-                    onClick={() => handleSwitchLanguage("en")}
-                  >
-                    <img
-                      src={gbFlag}
-                      alt=""
-                      className={`rounded-full w-[1.75rem] h-[1.75rem] border-2 border-tblue dark:border-light`}
-                    />
-                  </button>
-                )}
-              </div>
-            )} */}
-
             <button
               className="border-none bg-transparent text-tblue dark:text-orange cursor-pointer transition-all 1s ease-in-out"
               onClick={onToggleDarkMode}
